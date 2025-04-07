6bf681d0148d28d54074aca1488d8612d5030470
James-Hou22 <james.hou04@gmail.com>
2025-04-07 19:23:17 -0400

    reformatted code

--- web-app/app/routes.py
+++ web-app/app/routes.py
@@ -1,28 +1,35 @@
-# from flask import jsonify, render_template, request, redirect, url_for, session, flash
-# from bson.objectid import ObjectId
-# from app import app, mongo
+from flask import jsonify, render_template, request, redirect, url_for, session, flash
+from bson.objectid import ObjectId
+from app import app, mongo
 
-# @app.route("/")
-# def home():
-    # try:
-    #     items_collection = mongo.db.item
 
-    #     lost_items = list(items_collection.find({"status": "lost"}).sort("dateLost", -1).limit(5))
-    #     found_items = list(items_collection.find({"status": "found"}).
-            # sort("dateFound", -1).limit(5))
+@app.route("/")
+def home():
+    try:
+        items_collection = mongo.db.item
 
-    #     for item in lost_items + found_items:
-    #         item["_id"] = str(item["_id"])
+        lost_items = list(
+            items_collection.find({"status": "lost"}).sort("dateLost", -1).limit(5)
+        )
+        found_items = list(
+            items_collection.find({"status": "found"}).sort("dateFound", -1).limit(5)
+        )
 
-    #     recent_items = lost_items + found_items
-    #     recent_items.sort(key=lambda x: x.get("dateLost", x.get("dateFound")), reverse=True)
+        for item in lost_items + found_items:
+            item["_id"] = str(item["_id"])
 
-    #     return render_template("index.html", recent_items=recent_items[:3])
+        recent_items = lost_items + found_items
+        recent_items.sort(
+            key=lambda x: x.get("dateLost", x.get("dateFound")), reverse=True
+        )
 
-    # except Exception as e:
-    #     print(f"Error fetching recent items: {e}")
-    #     return render_template("index.html", recent_items=[])
-    # return
+        return render_template("index.html", recent_items=recent_items[:3])
 
-# if __name__ == '__main__':
-    # app.run(debug=True)
+    except Exception as e:
+        print(f"Error fetching recent items: {e}")
+        return render_template("index.html", recent_items=[])
+    return
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
