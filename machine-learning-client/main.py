a6086ee5663051e571818bbf9170ea8efe9e23c3
James-Hou22 <james.hou04@gmail.com>
2025-04-07 17:31:21 -0400

    linted main

--- machine-learning-client/main.py
+++ machine-learning-client/main.py
@@ -1,6 +1,13 @@
+"""
+Main file.
+"""
+
 def main():
+    """
+    Main Function.
+    """
     print("Machine Learning Client is running...")
 
 
 if __name__ == "__main__":
-    main() 
+    main()
