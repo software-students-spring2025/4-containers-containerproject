76208eb9de46dd03d403451497bf9c8c7cfd6ac6
maxlmeyring <max.luetkemeyring@nyu.edu>
2025-04-08 16:00:41 -0400

    entire setup for webpage, minus webpage styles

--- web-app/app/__init__.py
+++ web-app/app/__init__.py
@@ -1,31 +1,13 @@
-"""
-Init file.
-"""
-
-import os
 from flask import Flask
-from flask_pymongo import PyMongo
-from dotenv import load_dotenv
+from .db import init_db
 
-# load environment variables
-load_dotenv()
+def create_app():
+    app = Flask(__name__)
+    app.config.from_prefixed_env()
+    
+    init_db(app)
 
-app = Flask(__name__)
+    from .routes import main
+    app.register_blueprint(main)
 
-# configure MongoDB settings
-# app.config["MONGO_URI"] = os.getenv("MONGO_URI")
-
-# app.config["SECRET_KEY"] = os.urandom(24)
-
-# mongo = PyMongo(app)
-
-# #test the mongodb connection
-# try:
-#     mongo.cx.server_info()
-#     print("MongoDB connected successfully")
-# except Exception as e:
-#     print(f"Error connecting to MongoDB: {e}")
-#     raise e
-
-# #importing routes after app is created to avoid circular imports
-# from app import routes
+    return app
---
+++ web-app/app/db.py
@@ -0,0 +1,19 @@
+from flask import current_app, g
+from pymongo import MongoClient
+
+def init_db(app):
+    @app.before_request
+    def connect_db():
+        if "db" not in g:
+            mongo_uri = app.config.get("MONGO_URI")
+            g.client = MongoClient(mongo_uri)
+            g.db = g.client["ml_data"]
+
+    @app.teardown_appcontext
+    def close_db(error):
+        client = g.pop("client", None)
+        if client is not None:
+            client.close()
+
+def get_collection():
+    return g.db["results"]
