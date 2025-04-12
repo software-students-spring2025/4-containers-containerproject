244565eb7fa1e139a9b40c4513071d3f41c7184b
larryyang04 <larryyang2848@gmail.com>
2025-04-12 19:11:15 -0400

    black on conftest.py

--- web-app/tests/conftest.py
+++ web-app/tests/conftest.py
@@ -7,7 +7,8 @@ import os
 import pytest
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-from app import app as flask_app # pylint: disable=import-error, wrong-import-position
+from app import app as flask_app  # pylint: disable=import-error, wrong-import-position
+
 
 @pytest.fixture
 def client():
